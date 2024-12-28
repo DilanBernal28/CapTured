b7abb0f77ea34695cea2dc7aa6b0ca006cfbf0da
DilanBernal28 <162499949+DilanBernal28@users.noreply.github.com>
2024-12-27 19:07:28 -0500

    Angular

--- angular/CapturedStyles/src/app/app.component.ts
+++ angular/CapturedStyles/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { WeComponent } from "./features/we/we.component";
 import { CategoriesComponent } from "./features/categories/categories.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WeComponent, CategoriesComponent],
+  imports: [RouterOutlet, HeaderComponent, WeComponent, CategoriesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
