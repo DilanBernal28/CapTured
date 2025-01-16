fe5a63b00dd0f886e01442a95693e5b9858325c9
Gatuberk <dilanbernal01@gmail.com>
2025-01-15 23:32:44 -0500

    feat: add AppModule and implement API service with HTTP client

--- angular/CapturedStyles/src/app/services/api-service.service.ts
+++ angular/CapturedStyles/src/app/services/api-service.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,13 @@ import { Injectable } from '@angular/core';
 })
 export class ApiServiceService {
 
-  constructor() { }
+  url:string = 'http://localhost:3000/prd/productos';
+
+  constructor(private http: HttpClient) { }
+
+  getProductos(){
+    console.log(this.http.get(this.url));
+    return this.http.get(this.url);
+  }
+
 }
