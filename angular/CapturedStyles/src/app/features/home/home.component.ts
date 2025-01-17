67ae82a94d09898c4f7bb8fba6368f17b150f01d
Gatuberk <dilanbernal01@gmail.com>
2025-01-17 14:19:22 -0500

    feat: add products component and integrate it into home page

--- angular/CapturedStyles/src/app/features/home/home.component.ts
+++ angular/CapturedStyles/src/app/features/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { WeComponent } from "./we/we.component";
 import { CategoriesComponent } from "./categories/categories.component";
+import { ProductsComponent } from "./products/products.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WeComponent, CategoriesComponent],
+  imports: [WeComponent, CategoriesComponent, ProductsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- angular/CapturedStyles/src/app/features/home/products/products.component.ts
+++ angular/CapturedStyles/src/app/features/home/products/products.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
+import { IProduct } from '../../../interfaces/product.interface';
+import { IApiResponse } from '../../../interfaces/apiResponse.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  products:IProduct[] = []
 
+  constructor(private prodServ:ProductService){}
+
+  ngOnInit(){
+    try {
+      this.prodServ.getProductos().subscribe(
+        (response:IApiResponse<IProduct>) => {
+          this.products = response.body
+        }
+      )
+      console.log(this.products)
+    } catch (error) {
+      
+    }
+  }
 }
