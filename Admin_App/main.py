5f6efa4b5da24cb338ce0efc7cd3969ef06b2422
Gatuberk <dilanbernal01@gmail.com>
2025-01-22 10:41:22 -0500

    Admin App: feat:  Init db config

--- Admin_App/main.py
+++ Admin_App/main.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 import os
 from tkinter import filedialog
+from db_mysql import MySQL
 import shutil
 
 class MainApp:
@@ -18,6 +19,16 @@ ctk.set_appearance_mode("System")
 app = ctk.CTk()
 app.geometry("400x300")
 app.title("Guardar Imágenes")
+
+
+db = MySQL("localhost", "root", "12345678", "capturedstyles")
+db.connect()
+
+# Insertar datos
+db.execute_query("INSERT INTO usuarios (nombre, email) VALUES (%s, %s)", ("Juan", "juan@example.com"))
+
+# Consultar datos
+productos = db.fetch_all("SELECT * FROM product")
 
 # Función para seleccionar y guardar imágenes
 def guardar_imagen():
@@ -39,7 +50,7 @@ boton = ctk.CTkButton(app, text="Seleccionar Imagen", command=guardar_imagen)
 boton.pack(pady=20)
 
 # Etiqueta informativa
-label_info = ctk.CTkLabel(app, text="dfdf")
+label_info = ctk.CTkLabel(app, text=productos)
 label_info.pack(pady=10)
 
 # Iniciar aplicación
