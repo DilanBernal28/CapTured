a7a38dd2202deaa0939bfb7bcdbbbd5a7e1e2e85
Gatuberk <dilanbernal01@gmail.com>
2025-02-06 02:50:53 -0500

    feat: Register Password encoder

--- API/CapturedStyles/src/main/java/co/edu/ue/Auth/AuthService.java
+++ API/CapturedStyles/src/main/java/co/edu/ue/Auth/AuthService.java
@@ -1,13 +1,12 @@
 package co.edu.ue.Auth;
 
 import co.edu.ue.Jwt.JwtService;
-import co.edu.ue.dao.IUserJpa;
 import co.edu.ue.service.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import co.edu.ue.model.User;
 
-import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.Date;
 
@@ -15,10 +14,9 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class AuthService implements IAuthService{
 
-  private final IUserJpa repositoryService;
-
   private final IUserService userService;
   private final JwtService jwtService;
+  private final PasswordEncoder passwordEncoder;
 
   @Override
   public AuthResponse login(LoginRequest request) {
@@ -29,7 +27,7 @@ public class AuthService implements IAuthService{
   public AuthResponse register(RegisterRequest request) {
     User user = User.builder()
       .usrUsername(request.getUsrUsername())
-      .usrPassword(request.getUsrPassword())
+      .usrPassword(passwordEncoder.encode(request.getUsrPassword()))
       .usrEmail(request.getUsrEmail())
       .usrNombres(request.getUsrNombres())
       .usrApellidos(request.getUsrApellidos())
@@ -41,26 +39,9 @@ public class AuthService implements IAuthService{
       .usrFechaInicio(Date.from(Instant.now()))
       .build();
 
-      userService.addUser(user);
-//    try{
-//      repositoryService.save(user);
-//    } catch (Exception e) {
-//      throw new RuntimeException(e);
-//    }
-
-    try{
-      AuthResponse authResponse = AuthResponse.builder()
-        .token(jwtService.getToken(user))
-        .build();
-      return authResponse;
-    }catch(Exception e){
-      return AuthResponse.builder()
-        .token("malio sal")
-        .build();
-    }
-
-//    return AuthResponse.builder()
-//      .token(jwtService.getToken(user))
-//      .build();
+    userService.addUser(user);
+    return AuthResponse.builder()
+      .token(jwtService.getToken(user))
+      .build();
   }
 }
